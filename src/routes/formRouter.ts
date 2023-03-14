41c784366b6aaacb5c2034610e6b1557106b4cea
JV-Albuquerque-S <alb.albqrq@gmail.com>
2023-03-14 14:53:03 -0300

    feat: skeleton of form router

---
+++ src/routes/formRouter.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+//import {controller} from "../controller/...";
+
+const formRouter = Router();
+
+formRouter.post("/forms",/*controller*/);
+
+export default formRouter;
